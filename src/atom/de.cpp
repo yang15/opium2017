b867b1c208b7793891be77aa9b4902041894cd0f
yang15 <yang15@sas.upenn.edu>
2015-09-18 13:10:45 -0400

    modifications

--- src/atom/de.cpp
+++ src/atom/de.cpp
@@ -22,15 +22,35 @@ void ykf_(double *yk, int *rnli, int *rnlj, int *i, int *j, int *k, double *h, d
 void zkf_(double *yk, int *rnli, int *rnlj, int *i, int *j, int *k, double *h, double *z, double *r, int *np, int *lo, int *nmax);
 	}	
 
+<<<<<<< Updated upstream
 void de(params_t *params, double *y, double *x,double *t, double e, int l, double *p){
   //in : y,x,t
   //out: p
   // apply Method1 in Chapter7 of the book "The Hartree-Fock meth for atoms"
+=======
+void de(params_t *params, double *y, double *x,double *t, double e,int n, int l, double *p){
+  //in : y, x is the exchange, t is grange
+  //out: p stores the solved eigenvector
+  //control the solution of the differential equation 
+  // e is the orbital energy passing from scf.c <=> E(I,I) 
+  // n is the principle quantum number
+  // l is the angular momentum nubmer
+  // apply Method1 in Chapter7 of the book "The Hartree-Fock meth for atoms"
+  int norb = params->norb;
+  int ngrid = params->ngrid;
+  e =  new double *[norb];
+  for (int i_norb = 0; i_norb < norb; i_norb++){
+	  e[i_norb] = new double [norb];
+  }
+
+  e = params->etrial;
+>>>>>>> Stashed changes
   bool first = true;
   bool fail = false;
   double d0=0.0;
   double d2=2.0;
   double d5=0.5;
+<<<<<<< Updated upstream
 
   
     
@@ -53,6 +73,36 @@ double solve(params_t *params, int i, bool first, bool rel){
   	int norb = param->norb;
 	int ngrid = param->ngrid;
 	double *wnl = param->wnl;
+=======
+  
+  int i = i1; // i is the index of orbitals
+  double ed2 = e;
+  if (nwf == 1){kk = 2;}
+  node = n
+
+  
+    
+}
+
+
+int method1(params_t *params, double *y, double *x,double *t, double e, int l, double *p){
+	//initial guess of the eigenvalue of based on the hydrogenic wavefunction
+	double zval = params->z
+	double em = d0;
+	double eu = e;   // eigenvalue initial guess
+	double fu = eu;
+	int mk = 0;
+	solve
+
+}
+
+
+double solve(params_t *params, int i, bool first, bool rel){
+  	int norb = params->norb;
+	int ngrid = params->ngrid;
+	double *wnl = params->wnl;
+	double z = params->z;
+>>>>>>> Stashed changes
 	double *yr;
   	yr = (double *)malloc(ngrid*sizeof(double));
 	yr = y;
@@ -62,4 +112,8 @@ double solve(params_t *params, int i, bool first, bool rel){
 	if (first) {
 		potl_(yr, &i);
 		xch_(x, &i, 3);
+<<<<<<< Updated upstream
         	
+=======
+        	double zinf = max(0.05, z-yr)
+>>>>>>> Stashed changes
