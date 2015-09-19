9296a2e660dbd96ec048132742d6948be6c3aac3
yang15 <yang15@sas.upenn.edu>
2015-09-18 21:52:05 -0400

    updates

--- src/atom/de.cpp
+++ src/atom/de.cpp
@@ -20,14 +20,9 @@ void potl_(double *yr, int *i);
 void xch_(double *x, int *i, int *iopt);
 void ykf_(double *yk, int *rnli, int *rnlj, int *i, int *j, int *k, double *h, doub *z, double *r, int *lo, int *nmax);
 void zkf_(double *yk, int *rnli, int *rnlj, int *i, int *j, int *k, double *h, double *z, double *r, int *np, int *lo, int *nmax);
+void quad_(int *i, int *m, double *yr, double *x);
 	}	
 
-<<<<<<< Updated upstream
-void de(params_t *params, double *y, double *x,double *t, double e, int l, double *p){
-  //in : y,x,t
-  //out: p
-  // apply Method1 in Chapter7 of the book "The Hartree-Fock meth for atoms"
-=======
 void de(params_t *params, double *y, double *x,double *t, double e,int n, int l, double *p){
   //in : y, x is the exchange, t is grange
   //out: p stores the solved eigenvector
@@ -38,21 +33,85 @@ void de(params_t *params, double *y, double *x,double *t, double e,int n, int l,
   // apply Method1 in Chapter7 of the book "The Hartree-Fock meth for atoms"
   int norb = params->norb;
   int ngrid = params->ngrid;
-  e =  new double *[norb];
-  for (int i_norb = 0; i_norb < norb; i_norb++){
-	  e[i_norb] = new double [norb];
-  }
 
-  e = params->etrial;
->>>>>>> Stashed changes
   bool first = true;
   bool fail = false;
+  bool ilast = true;
   double d0=0.0;
+  double d1=1.0;
   double d2=2.0;
   double d5=0.5;
-<<<<<<< Updated upstream
 
-  
+  int ind[norb];
+  int m, node, mk, kk, nj;
+  m = l;
+  // m is the angular quantum number for each orbital
+  // kk is imeth or meth(m)
+  double p2[ngrid], hq[ngrid], xx[ngrid];
+  double v, b4, cn, c, xy, xp, azz, pp, fn, em, fm, eu,fu, delta_e;
+
+   double ed2 = e;
+   if (norb == 1){kk = 2;} 
+   int node = n - m - 1; 
+// local variables
+   int i, j, nn, ij, jj;
+   double ed2, pn , ed1, cd, dp, diff, dpw, pnn;
+   bool change;
+
+// call method1 to solve the differential equation
+//
+    method1(params, y, x, t, e, l, p,m, azz, fail,iprint);
+    if (fail) {
+        // go to 25
+    }
+
+    double pn = sqrt(quad_(&i,&m,p, p));
+    for (int i_p=0; i_p < ngrid; i_p++){
+        p[i_p] = p[i_p] / pn;
+    }
+    azz = azz/pnn;
+
+// check if different method should be used
+//
+    if (kk==1){
+        if ( (abs(1.0-ed2/e) < 0.005) && max(abs(d1-pn),abs(d1/pn - d1))) {
+            kk = 2;
+            // go to 25
+        } else {
+            if ( (abs(d1-ed2/e) < 0.0001) && (ic > 1)) {
+                ic = ic - 1;
+            }
+        }
+// set the accelerating parameter
+// acc[norb] is the accelerating parameter in method2 alpha_i P289
+    if (ipr != m) {
+        acc[m] = 0.75 * acc[m];
+    } else {
+        ed2 = ed2 - e;
+        if (ed1 * ed2 > d0) {
+            acc[m] = 0.75 * acc[m];
+        } else {
+            acc[m] = (d1 + d3*acc[m])/d4;
+        }
+    }
+    c = acc[m];
+    cd = d1 - c;
+// in our code but not in the original code
+    if (acc[m] > 0.99999) {
+        acc[m] = 0.5;
+    }
+// Improve the estimates
+//
+    dp = d0;
+    for (int i_estm=1; i_estm<=m; i_estm++ ){
+        diff = p(i_estm) - pde(i_estm);
+        dp = max(dp, abs(diff)*(r2(i_estm)));
+        p(i_estm) = pde(i_estm) + c * diff;
+    }
+    if (m != ngrid) {
+        m = m + 1;
+        p
+
     
 }
 
@@ -73,7 +132,6 @@ double solve(params_t *params, int i, bool first, bool rel){
   	int norb = param->norb;
 	int ngrid = param->ngrid;
 	double *wnl = param->wnl;
-=======
   
   int i = i1; // i is the index of orbitals
   double ed2 = e;
@@ -102,7 +160,6 @@ double solve(params_t *params, int i, bool first, bool rel){
 	int ngrid = params->ngrid;
 	double *wnl = params->wnl;
 	double z = params->z;
->>>>>>> Stashed changes
 	double *yr;
   	yr = (double *)malloc(ngrid*sizeof(double));
 	yr = y;
@@ -112,8 +169,5 @@ double solve(params_t *params, int i, bool first, bool rel){
 	if (first) {
 		potl_(yr, &i);
 		xch_(x, &i, 3);
-<<<<<<< Updated upstream
         	
-=======
         	double zinf = max(0.05, z-yr)
->>>>>>> Stashed changes
