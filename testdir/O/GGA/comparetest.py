246eb34e96527a86f0c080523dea75cf99f76a9f
yang15 <yang15@sas.upenn.edu>
2018-02-12 16:11:31 -0500

    test via python 5

--- testdir/O/GGA/comparetest.py
+++ testdir/O/GGA/comparetest.py
@@ -6,33 +6,25 @@ import os
 import numpy as np
 import pytest
 
-tol = 1e-4
+def test_alleEng():
+    tol = 1e-6
 
-eigvalues=[]
-filename=open("alleEng")
-for line in filename:
-    """
-    split the file into eigenvalues
-    the third column data
-    """
-   words=line.split()
-   eigvalues.append(float(words[2]))
-   
-filename.close()
+    eigvalues=[]
+    filename=open("alleEng")
+    for line in filename:
+        words=line.split()
+        eigvalues.append(float(words[2]))
+    filename.close()
 
 """
 Comparison file data 
 """
-i=0
-compfilename=open("reference/alleEng")
-for line in filename:
-    """
-    split the file into eigenvalues
-    the third column data
-    """
-   words=line.split()
-   assert np.allclose(eigvalues[i], float(words[2]), tol)
-   i=i+1
+    i=0
+    compfilename=open("reference/alleEng")
+    for line in compfilename:
+        words=line.split()
+    assert np.allclose(eigvalues[i], float(words[2]), tol)
+    i=i+1
    
 filename.close()
 
